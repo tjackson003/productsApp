4fc5598a37d308431a4156efd12cd92764dbb4fa
TJ Jackson <elitefourfresh@gmail.com>
2019-09-03 13:54:37 -0400

    controllers

---
+++ controllers/product.controllers.js
@@ -0,0 +1,7 @@
+const Product = require("../models/product.model");
+
+// SIMPLE VERSION, WITHOUT VALIDATION OR SANITATION
+
+exports.test = function (req, res) {
+    res.send("Greetings from the Test controller!");
+};
