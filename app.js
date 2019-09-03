3121ab7cd18cc74b30df6d71191bf3edc661b134
TJ Jackson <elitefourfresh@gmail.com>
2019-09-03 13:22:35 -0400

    dedicated port

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ const bodyParser = require("body-parser");
 // INITIALIZE OUR EXPRESS APP
 
 const app = express();
+
+// DEDICATING PORT NUMBER; TELLING EXPRESS TO LISTEN TO PORT
+
+let port = 3000;
+
+app.listen(port, (e) => {
+    if(e) throw e;
+
+    console.log("Server is up and running on port number " + port);
+});
